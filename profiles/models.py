ee744ccb97015a62db58478fadfaf2a2be351e05
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-03-17 22:27:53 +0300

    added post models

--- profiles/models.py
+++ profiles/models.py
@@ -4,4 +4,15 @@ from signups.models import Signups
 # Create your models here.
 class Profiles(models.Model):
     users = models.ForeignKey(Signups, on_delete = models.CASCADE)
-    userimages = models.ImageField()
+    userimages = models.ImageField(upload_to = 'profiles/')
+    aboutme = models.TextField()
+
+    class Meta:
+        db_table = 'Profiles'
+
+    def __str__(self):
+        return self.users
+
+class Posts(models.Model):
+    postby = models.ForeignKey(Signups, on_delete = models.CASCADE)
+    post_image = models.ImageField(upload_to = 'posts/')
