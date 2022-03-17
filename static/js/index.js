62c174331c91419782db4abf14b8a3024a1c6218
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-03-17 17:00:59 +0300

    added remember me functionality

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,36 @@
 const removeError = (clicked_id) =>{
-     document.getElementById(clicked_id).style.border = "none";
+    
+    document.getElementById(clicked_id).style.border = "none";
 }
 
 const ValidateSignin = () =>{
     let useremail = document.getElementById("useremail");
-    let passwords = document.getElementById("rememberme");
+    let passwords = document.getElementById("userpassword");
+    let rememberme = document.getElementById("rememberme");
+    if(useremail.value.trim() === "" || passwords.value.trim() === ""){
+        if(useremail.value.trim() === ""){
+            useremail.style.border = "1px solid red";
+            return false;
+        }else{
+            passwords.style.border = "1px solid red";
+            return false;
+        }
+    }else{
+        if(rememberme.checked){
+            alert("checked")
+            localStorage.setItem("useremail", useremail);
+            localStorage.setItem("passwords", passwords);
+        }
+    }
+
 }
+
+const getcredentials = () =>{
+    let userem = localStorage.getItem("useremail");
+    let userpass = localStorage.getItem("passwords");
+    document.getElementById("useremail").value = userem;
+    document.getElementById("userpassword").value = userpass;
+    
+    setTimeout("getcredentials", 100);
+}
+window.onload = getcredentials;
