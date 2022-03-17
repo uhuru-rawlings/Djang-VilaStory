488042123c060c68c621586f21b2965dc4eedaf9
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-03-17 22:21:55 +0300

    added profiles model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from signups.models import Signups
 
 # Create your models here.
+class Profiles(models.Model):
+    users = models.ForeignKey(Signups, on_delete = models.CASCADE)
+    userimages = models.ImageField()
