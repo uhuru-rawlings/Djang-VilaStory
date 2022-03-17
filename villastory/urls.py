1c0144a3b3dfdb4062b28d416fbc486a49de218b
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-03-17 23:40:57 +0300

    added add bio models

--- villastory/urls.py
+++ villastory/urls.py
@@ -4,6 +4,8 @@ from signups.views import signup_view,signup_meth
 from logins.views import login_view,login_meth
 from profiles.views import profile_view,logout_view
 from posts.views import post_view
+from django.contrib.staticfiles.urls import static
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,4 +17,4 @@ urlpatterns = [
     path('profile/',profile_view, name="profile"),
     path('logout/',logout_view, name="logout"),
    
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
