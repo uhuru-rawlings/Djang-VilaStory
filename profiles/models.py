1a7f41a7075346c476567bfe78c6994fe78df53c
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-03-17 22:35:35 +0300

    eddited return statement

--- profiles/models.py
+++ profiles/models.py
@@ -11,10 +11,16 @@ class Profiles(models.Model):
         db_table = 'Profiles'
 
     def __str__(self):
-        return self.users
+        return self.aboutme
 
 class Posts(models.Model):
     postby = models.ForeignKey(Signups, on_delete = models.CASCADE)
-    post_image = models.ImageField(upload_to = 'posts/')
+    post_image = models.ImageField(upload_to = 'posts/', null = True)
     post_text = models.TextField()
     post_dates = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'Posts'
+
+    def __str__(self):
+        return self.post_text
