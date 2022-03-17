32a144f5939f18ce8dac56b4e0c4541f7024d9c7
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-03-17 22:48:28 +0300

    added signup methods

--- signups/views.py
+++ signups/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from signups.models import Signups
 # Create your views here.
 def signup_view(request):
@@ -13,4 +13,12 @@ def signup_meth(request):
         phonenumber = request.POST['phonenumber']
         uservillage = request.POST['userimages']
         userpassword = request.POST['userpassword']
-        useremails = request.POST['useremail']
+        try:
+            # check if username exists
+            users = Signups.objects.get(useremail=useremails)
+            errors = 'user with this email already exist, please try login in.'
+            return redirect('/')
+        except:
+            new_user = Signups(useremail = useremails, phone = phonenumber, village = uservillage, passwords = userpassword)
+            new_user.save()
+            return redirect('/')
