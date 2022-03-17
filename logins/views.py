5ed2230565befefc0ed99889a616567c148fd39c
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-03-17 22:58:09 +0300

    added login

--- logins/views.py
+++ logins/views.py
@@ -2,13 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 def login_view(request):
+    errors = ''
+    if request.method == 'POST':
+        useremails = request.POST['useremail']
+        userpassword = request.POST['userpassword']
+        try:
+            users = Signups.objects.get(useremail=useremails)
+            if users.passwords == userpassword:
+                response = redirect("/home/")
+                response.create_cookie("logedin", useremails)
+                return response
+            else:
+                errors = 'Wrong password please try again'
+                return redirect("/")
+        except:
+            errors = "user dont exist, please try gain"
     context = {
         'title':'vilastory | login',
+        'errors':errors
     }
     return render(request, "login.html",context)
 
 def login_meth(request):
-   if request.method == 'POST':
-        useremails = request.POST['useremail']
-        phonenumber = request.POST['phonenumber']
-        users = Signups.objects.get(useremail=useremails)
+   pass
