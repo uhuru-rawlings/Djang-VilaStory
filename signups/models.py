83c3a1fddc91ccd552dfa65e4de9cb5db2b0a0a1
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-03-17 17:58:26 +0300

    added registration models

--- signups/models.py
+++ signups/models.py
@@ -2,4 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Signups(models.Model):
-    useremail
+    useremail = models.CharField()(max_length=200)
+    phone = models.CharField(max_length=200)
+    village = models.CharField(max_length=200)
+    passwords = models.CharField(max_length=300)
+
+    class Meta:
+        db_table = 'Signups'
+
+    def __str__(self):
+        return self.useremail
