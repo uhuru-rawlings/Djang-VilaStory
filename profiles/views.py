2d972d5d7b4b4fcaf29dacbf658478d188cfe70b
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-03-17 23:24:25 +0300

    added profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, redirect
-
+from signups.models import Signups
 # Create your views here.
 def profile_view(request):
+    try:
+        user = request.COOKIES['logedin']
+    except:
+        return redirect("/")
+    users = Signups.objects.filter(useremail = user).first()
+    print("###########################")
+    print(users.id)
+    print("###########################")
     context = {
         'title':'vilastory | profiles',
+        'users':users
     }
-    return render(request, "posts.html",context)
+    return render(request, "profiles.html",context)
 
 
 def logout_view(request):
