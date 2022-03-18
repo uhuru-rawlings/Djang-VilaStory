dd3a516382e6070254340d757ead13248fa316fd
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-03-18 09:41:05 +0300

    added url

--- villastory/urls.py
+++ villastory/urls.py
@@ -6,12 +6,14 @@ from profiles.views import profile_view,logout_view,savebio_view,addpost_view,ad
 from posts.views import post_view
 from django.contrib.staticfiles.urls import static
 from . import settings
+from posts.models import ImportantInfo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',login_view, name="login"),
     path('signin',login_meth, name="signin"),
     path('signup/',signup_view, name="signup"),
+    path('addinfo/',ImportantInfo, name="addinfo"),
     path('create_account/',signup_meth, name="create_account"),
     path('home/',post_view, name="homepage"),
     path('profile/',profile_view, name="profile"),
