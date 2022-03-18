044b72e0e1bd5e5969bc2309b78e1520db4d5c94
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-03-18 08:08:50 +0300

    added get post

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-
+from profiles.models import Posts
 # Create your views here.
 def post_view(request):
+    try:
+        user = request.COOKIES['logedin']
+    except:
+        return redirect("/")
+    users = Signups.objects.filter(useremail = user).first()
+    try:
+        get_posts = Posts.objects.filter(village = users.village)
+    except:
+        get_posts = "noposts"
     context = {
         'title':'vilastory | posts',
+        'get_posts':get_posts
     }
     return render(request, "posts.html",context)
