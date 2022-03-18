f7f817a9744d5bfe0a285de3b32aa10b0d7ab5eb
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-03-18 06:22:45 +0300

    added how to delete cookies

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from signups.models import Signups
+from .models import Profiles,Posts
 # Create your views here.
 def profile_view(request):
     try:
@@ -14,12 +15,31 @@ def profile_view(request):
     }
     return render(request, "profiles.html",context)
 
-def logout_view(request):
+def savebio_view(request):
     if request.method == 'POST':
         userimages = request.FILES['userimage']
         biotext = request.POST['userimage']
+
+        user = request.COOKIES['logedin']
+        get_user = Signups.objects.get(useremail= user)
+        exist_page = Profiles.objects.filter(users = get_user).exists()
+        if exist_page:
+             about_me = Profiles.objects.get(users = get_user)
+             about_me.userimages = userimages
+             about_me.aboutme = biotext
+             about_me.save()
+        else:
+            about_me = Profiles(users=get_user,userimage = userimages, aboutme=biotext)
+            about_me.save()
+
     return redirect('/')
 
 
 def logout_view(request):
+    try:
+        user = request.COOKIES['logedin']
+        respose = redirect("/")
+        respose.delete_cookie("logedin")
+    except:
+        return redirect("/")
     return redirect('/')
