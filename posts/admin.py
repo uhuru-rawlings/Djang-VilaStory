bffd21c5c65a2cd0cbd83485712a52927415ab03
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-03-18 10:46:30 +0300

    eddited village model

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from posts.models import ImportantInfo
 # Register your models here.
+
+admin.site.register(ImportantInfo)
--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,7 @@ from profiles.models import Profiles
 from signups.models import Signups
 # Create your models here.
 class ImportantInfo(models.Model):
-    village = models.ForeignKey(Signups, on_delete = models.CASCADE)
+    village = models.ForeignKey(Signups,verbose_name='village', on_delete = models.CASCADE)
     hospital = models.CharField(max_length=300)
     policestation = models.CharField(max_length=300)
     market = models.CharField(max_length=300)
